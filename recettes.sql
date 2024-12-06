0c08973ec1a1770dfaabb991dcb34e2d4277057d
Anne-DEHAEZE <anne.dehaeze@yahoo.fr>
2024-12-06 17:20:31 +0100

    recettes

--- recettes.sql
+++ recettes.sql
@@ -22,12 +22,12 @@ USE `recette_anne`;
 -- Listage de la structure de table recette_anne. categorie
 CREATE TABLE IF NOT EXISTS `categorie` (
   `id_categorie` int NOT NULL AUTO_INCREMENT,
-  `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL DEFAULT '',
+  `nomCategorie` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id_categorie`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 -- Listage des données de la table recette_anne.categorie : ~3 rows (environ)
-INSERT INTO `categorie` (`id_categorie`, `type`) VALUES
+INSERT INTO `categorie` (`id_categorie`, `nomCategorie`) VALUES
 	(1, 'Entree'),
 	(2, 'Plat'),
 	(3, 'Dessert');
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `ingredient` (
   `nomIngredient` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL DEFAULT '',
   `prix` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_ingredient`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
--- Listage des données de la table recette_anne.ingredient : ~35 rows (environ)
+-- Listage des données de la table recette_anne.ingredient : ~38 rows (environ)
 INSERT INTO `ingredient` (`id_ingredient`, `nomIngredient`, `prix`) VALUES
 	(1, 'Tomates', 1.2),
 	(2, 'Mozzarella', 2.5),
@@ -53,7 +53,7 @@ INSERT INTO `ingredient` (`id_ingredient`, `nomIngredient`, `prix`) VALUES
 	(9, 'Noix', 2.3),
 	(10, 'Salade', 1.5),
 	(11, 'Concombre', 2),
-	(12, 'Yahourt nature', 0.8),
+	(12, 'Yahourt nature', 50),
 	(13, 'Ail', 0.5),
 	(14, 'Menthe', 0.7),
 	(15, 'Poivrons', 1.4),
@@ -76,7 +76,10 @@ INSERT INTO `ingredient` (`id_ingredient`, `nomIngredient`, `prix`) VALUES
 	(32, 'Beurre', 1.5),
 	(33, 'Cannelle', 1),
 	(34, 'Farine', 0.8),
-	(35, 'Confiture de fraises', 2.4);
+	(35, 'Confiture de fraises', 2.4),
+	(37, 'Lardons', 3.5),
+	(38, 'licorne', 50),
+	(39, 'Poivre', 2.5);
 
 -- Listage de la structure de table recette_anne. ingredientparrecette
 CREATE TABLE IF NOT EXISTS `ingredientparrecette` (
@@ -90,61 +93,57 @@ CREATE TABLE IF NOT EXISTS `ingredientparrecette` (
   CONSTRAINT `FK_ingredientparrecette_recette` FOREIGN KEY (`id_recette`) REFERENCES `recette` (`id_recette`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
--- Listage des données de la table recette_anne.ingredientparrecette : ~2 rows (environ)
+-- Listage des données de la table recette_anne.ingredientparrecette : ~49 rows (environ)
 INSERT INTO `ingredientparrecette` (`quantite`, `uniteMesure`, `id_ingredient`, `id_recette`) VALUES
 	(2, 'tomates', 1, 1),
 	(1, 'boule', 2, 1),
-	(0.5, 'botte', 3, 1), 
-	(2, 'cuillères à soupe', 4, 1), 
-	(1, 'cuillère à soupe', 5, 1), 
-
+	(0.5, 'botte', 3, 1),
+	(2, 'cuillères à soupe', 4, 1),
+	(1, 'cuillère à soupe', 5, 1),
 	(4, 'tranches', 6, 2),
 	(100, 'g', 7, 2),
 	(1, 'cuillère à soupe', 8, 2),
 	(30, 'g', 9, 30),
 	(1, 'poignée', 10, 2),
-
-	(2, 'pièces', 11, 3), 
+	(2, 'pièces', 11, 3),
 	(200, 'ml', 12, 3),
-	(1, 'gousse', 13, 3), 
-	(5, 'feuilles', 14, 3), 
-
-	(1, 'pièce', 15, 4), 
-	(1, 'pièce', 16, 4), 
-	(1, 'pièce', 17, 4), 
-	(100, 'g', 18, 4), 
+	(1, 'gousse', 13, 3),
+	(5, 'feuilles', 14, 3),
+	(1, 'pièce', 15, 4),
+	(1, 'pièce', 16, 4),
+	(1, 'pièce', 17, 4),
+	(100, 'g', 18, 4),
 	(2, 'cuillères à soupe', 4, 4),
-
-	(250, 'g', 19, 5), 
-	(200, 'ml', 20, 5),  
-	(5, 'feuilles', 3, 5), 
-
-	(100, 'g', 22, 6), 
-	(3, 'pièces', 21, 6), 
-	(50, 'g', 23, 6), 
-
-	(200, 'g', 24, 7), 
-	(150, 'g', 25, 7), 
+	(250, 'g', 19, 5),
+	(200, 'ml', 20, 5),
+	(5, 'feuilles', 3, 5),
+	(100, 'g', 22, 6),
+	(3, 'pièces', 21, 6),
+	(50, 'g', 23, 6),
+	(200, 'g', 24, 7),
+	(150, 'g', 25, 7),
 	(2, 'cuillères à soupe', 26, 7),
-	(200, 'ml', 27, 7), 
-	(1, 'pièce', 28, 7), 
-
-	(1, 'pièce', 29, 8), 
-	(3, 'pièces', 30, 8), 
-	(50, 'g', 31, 8), 
-	(30, 'g', 32, 8), 
-	(1, 'cuillère à café', 33, 8), 
-
-	(200, 'g', 34, 9), 
-	(2, 'pièces', 21, 9), 
-	(300, 'ml', 12, 9), 
+	(200, 'ml', 27, 7),
+	(1, 'pièce', 28, 7),
+	(1, 'pièce', 29, 8),
+	(3, 'pièces', 30, 8),
+	(50, 'g', 31, 8),
+	(30, 'g', 32, 8),
+	(1, 'cuillère à café', 33, 8),
+	(200, 'g', 34, 9),
+	(2, 'pièces', 21, 9),
+	(300, 'ml', 12, 9),
 	(50, 'g', 35, 9),
-
-	(4, 'pièces', 30, 10), 
-	(30, 'g', 31, 10), 
-	(1, 'cuillère à café', 33, 10);
-
-
+	(4, 'pièces', 30, 10),
+	(30, 'g', 31, 10),
+	(1, 'cuillère à café', 33, 10),
+	(0.2, 'Lardons', NULL, NULL),
+	(200, 'g', 37, 12),
+	(500, 'g', 19, 12),
+	(4, 'oeufs', 21, 12),
+	(1, 'licorne', 38, 15),
+	(1, 'gousse', 13, 5),
+	(1, 'g', 39, NULL);
 
 -- Listage de la structure de table recette_anne. recette
 CREATE TABLE IF NOT EXISTS `recette` (
@@ -152,24 +151,26 @@ CREATE TABLE IF NOT EXISTS `recette` (
   `nomRecette` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL DEFAULT '0',
   `tempsPrepa` int NOT NULL DEFAULT '0',
   `instructions` text CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci,
-  `id_categorie` int NOT NULL,
+  `id_categorie` int DEFAULT NULL,
   PRIMARY KEY (`id_recette`),
   KEY `id-categorie` (`id_categorie`) USING BTREE,
   CONSTRAINT `FK_recette_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
--- Listage des données de la table recette_anne.recette : ~10 rows (environ)
+-- Listage des données de la table recette_anne.recette : ~12 rows (environ)
 INSERT INTO `recette` (`id_recette`, `nomRecette`, `tempsPrepa`, `instructions`, `id_categorie`) VALUES
-	(1, 'Salade tomate mozza', 20, 'Couper les tomates en tranches, ajouter la mozza, le basilic et assaisonner avec l\'huile d\'olive, sel et poivre', 0),
-	(2, 'Tartines de formage de chèvre et miel', 25, 'Faire griller le pain de campagne, ajouter du fromage de chèvre, du miel et des noix, garnir de roquette', 0),
-	(3, 'Soupe froide de concombre et yahourt', 30, 'Mixer le concombre, ajouter le yahourt nature, l\'ail et la menthe, assaisonner avec set et poivre', 0),
-	(4, 'Salade de quinoa et légumes grillés', 40, 'Cuire le quinoa et faire griller les légumes (poivrons, courgettes, aubergones). mélanger la feta et assaisonner avec l\'huile d\'olive, sel et poivre.', 0),
-	(5, 'Pâtes sauce tomate et basilic', 25, 'Cuire les pâtes, chauffer la sauce tomate avec l\'ail et le basilic puis mélanger avec les pâtes', 0),
-	(6, 'Omelette aux épinards et formage râpé', 20, 'Cuire les épinards dans une poêle, ajouter les oeufs battus et le fromage râpés, cuire jusqu\'à ce que l\'omelette soit bien dorée', 0),
-	(7, 'Poulet curry avec riz basmati', 45, 'Cuire le poulet dans le curry en poudre et le lait de coco, servir avec du riz basmati.', 0),
-	(8, 'Tarte aux pommes simple', 50, 'Cuire la pâte brisée, disposer les pommes tranchées, saupoudrer de sucre et de canelle, puis cuir au four', 0),
-	(9, 'Crêpes sucrées à la confiture de fraises', 30, 'Préparer la pâte à crêpes, cuire les crêpes puis les garnie de confiture de fraises', 0),
-	(10, 'Compote de pommes maison', 40, 'Faire cuire les pommes avec du sucre et de la canelle puis mixer pour obtenir une compote', 0);
+	(1, 'Salade tomate mozza', 15, 'Couper les tomates en tranches, ajouter la mozza, le basilic et assaisonner avec l\'huile d\'olive, sel et poivre', 1),
+	(2, 'Tartines de formage de chèvre et miel', 20, 'Faire griller le pain de campagne, ajouter du fromage de chèvre, du miel et des noix, garnir de roquette', 1),
+	(3, 'Gloubiboulga concombre et yahourt', 25, 'Mixer le concombre, ajouter le yahourt nature, l\'ail et la menthe, assaisonner avec set et poivre', 1),
+	(4, 'Salade de quinoa et légumes grillés', 35, 'Cuire le quinoa et faire griller les légumes (poivrons, courgettes, aubergones). mélanger la feta et assaisonner avec l\'huile d\'olive, sel et poivre.', 2),
+	(5, 'Pâtes sauce tomate et basilic', 20, 'Cuire les pâtes, chauffer la sauce tomate avec l\'ail et le basilic puis mélanger avec les pâtes', 2),
+	(6, 'Omelette aux épinards et formage râpé', 15, 'Cuire les épinards dans une poêle, ajouter les oeufs battus et le fromage râpés, cuire jusqu\'à ce que l\'omelette soit bien dorée', 2),
+	(7, 'Poulet curry avec riz basmati', 40, 'Cuire le poulet dans le curry en poudre et le lait de coco, servir avec du riz basmati.', 2),
+	(8, 'Tarte aux pommes simple', 45, 'Cuire la pâte brisée, disposer les pommes tranchées, saupoudrer de sucre et de canelle, puis cuir au four', 3),
+	(9, 'Crêpes sucrées à la confiture de fraises', 25, 'Préparer la pâte à crêpes, cuire les crêpes puis les garnie de confiture de fraises', 3),
+	(10, 'Compote de pommes maison', 35, 'Faire cuire les pommes avec du sucre et de la canelle puis mixer pour obtenir une compote', 3),
+	(12, 'Pâte carbonara', 15, 'Faire cuire les pâtes, pendant ce temps faire revenir les lardons et battre les oeufs en omelette. Une fois l\'ensemble cuit, mélanger le tout et refaire un peu cuire.', 3),
+	(16, 'Eau chaude', 2, 'Verser de l\'eau chaude dans une tasse', 2);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
---
+++ requetes.sql
@@ -0,0 +1,198 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!NE PAS OUBLIER DE REFRESH!!!!!!!!!!!!!!!!!!!!!!!!!!! après les insert into
+
+-- 1 Afficher toutes les recettes disponibles (nom de la recette, catégorie et temps de préparation) triées 
+de façon décroissante sur la durée de réalisation
+
+SELECT r.nomRecette, r.tempsPrepa, c.nomCategorie
+FROM recette r
+INNER JOIN categorie c
+ON r.id_categorie = c.id_categorie
+
+-- 2 En modifiant la requête précédente, faites apparaître le nombre d’ingrédients nécessaire par recette
+SELECT COUNT(ir.id_ingredient) AS nbIngredient, nomRecette
+FROM ingredientparrecette ir
+INNER JOIN recette r
+ON r.id_recette = ir.id_recette
+GROUP BY nomRecette
+
+
+-- 3 Afficher les recettes qui nécessitent au moins 30 min de préparation
+SELECT r.nomRecette, r.tempsPrepa
+FROM recette r
+WHERE r.tempsPrepa >= 30
+
+
+
+-- 4 Afficher les recettes dont le nom contient le mot « Salade » (peu importe où est situé le mot en 
+question)
+SELECT r.nomRecette
+FROM recette r
+WHERE r.nomRecette LIKE '%salade%';
+
+
+-- 5 Insérer une nouvelle recette : « Pâtes à la carbonara » dont la durée de réalisation est de 20 min avec 
+--les instructions de votre choix. Pensez à alimenter votre base de données en conséquence afin de 
+--pouvoir lister les détails de cette recettes (ingrédients)
+
+INSERT INTO recette (nomRecette, tempsPrepa, instructions, id_categorie)
+VALUES ('Pâte carbonara', 20, 'Faire cuire les pâtes, pendant ce temps faire revenir les lardons et battre les oeufs en omelette. Une fois l\'ensemble cuit, mélanger le tout et refaire un peu cuire.', 3);
+
+INSERT INTO ingredientparrecette (quantite, uniteMesure, id_ingredient, id_recette)
+VALUES (4, 'oeufs', 21, 12);
+
+INSERT INTO ingredientparrecette (quantite, uniteMesure, id_ingredient, id_recette)
+VALUES (500, 'g', 19, 12);
+
+INSERT INTO ingredientparrecette (quantite, uniteMesure, id_ingredient, id_recette)
+VALUES (200, 'g', 37, 12);
+
+
+
+-- 6 Modifier le nom de la recette ayant comme identifiant id_recette = 3 (nom de la recette à votre 
+convenance)
+
+UPDATE recette 
+SET nomRecette = 'Gloubiboulga concombre et yahourt'
+WHERE id_recette=3;
+
+
+-- 7 Supprimer la recette n°15 de la base de données
+
+DELETE recette FROM recette r
+INNER JOIN ingredientparrecette ir
+ON r.id_recette = ir.id_recette
+INNER JOIN ingredient i
+ON ir.id_ingredient = i.id_ingredient
+WHERE r.id_recette = 15;
+
+trouvé dans la documentation cette méthode mais reste à approfondir  
+ALTER TABLE  ingredientparrecette ir
+ADD CONSTRAINT id_ingredient
+FOREIGN KEY (id_recette) REFERENCES recette(id_recette) ON DELETE CASCADE;
+
+
+
+-- 8 Afficher le prix total de la recette n°5
+
+SELECT SUM(i.prix * ir.quantite) AS prixTotal
+FROM ingredient i
+INNER JOIN ingredientparrecette ir
+ON i.id_ingredient = ir.id_ingredient
+WHERE id_recette = 5;
+
+
+-- 9 Afficher le détail de la recette n°5 (liste des ingrédients, quantités et prix)
+SELECT  nomIngredient,
+quantite,
+SUM(i.prix * ir.quantite) AS prixTotal
+FROM ingredient i
+INNER JOIN ingredientparrecette ir
+ON i.id_ingredient = ir.id_ingredient
+WHERE id_recette = 5;
+
+GROUP BY nomIngredient, quantite
+
+
+
+-- 10 Ajouter un ingrédient en base de données : Poivre, unité : cuillère à café, prix : 2.5 €
+INSERT INTO ingredient (nomIngredient, prix)
+VALUES ('Poivre', 2.5);
+
+INSERT INTO ingredientparrecette (quantite, uniteMesure, id_ingredient)
+VALUES (1, 'g', 39);
+
+-- 11 Modifier le prix de l’ingrédient n°12 (prix à votre convenance)
+UPDATE ingredient
+SET prix = 50
+WHERE id_ingredient = 12;
+
+-- 12 Afficher le nombre de recettes par catégories : X entrées, Y plats, Z desserts
+SELECT c.nomCategorie, 
+COUNT(id_recette) AS nombre
+FROM recette r
+INNER JOIN categorie c
+ON r.id_categorie = c.id_categorie
+GROUP BY nomCategorie;
+
+-- 13 Afficher les recettes qui contiennent l’ingrédient « Pâtes »
+SELECT nomRecette
+FROM recette r
+WHERE nomRecette LIKE '%Pâte%';
+
+-- 14 Mettez à jour toutes les recettes en diminuant leur temps de préparation de 5 minutes 
+UPDATE recette
+SET tempsPrepa = tempsPrepa - 5;
+
+
+-- 15 Afficher les recettes qui ne nécessitent pas d’ingrédients coûtant plus de 2€ par unité de mesure
+
+SELECT nomRecette
+FROM recette r
+WHERE r.id_recette NOT IN (
+    SELECT r.id_recette
+    FROM recette r
+    INNER JOIN ingredientparrecette ir 
+	 ON r.id_recette = ir.id_recette
+	 INNER JOIN ingredient i
+	 ON i.id_ingredient = ir.id_ingredient
+    WHERE i.prix > 2
+);
+
+
+-- 16 Afficher la / les recette(s) les plus rapides à préparer
+
+SELECT nomRecette, tempsPrepa
+FROM recette 
+WHERE tempsPrepa = (
+    SELECT MIN(tempsPrepa) FROM recette
+);
+
+
+-- 17 Trouver les recettes qui ne nécessitent aucun ingrédient (par exemple la recette de la tasse d’eau 
+-- chaude qui consiste à verser de l’eau chaude dans une tasse)
+
+SELECT r.nomRecette
+FROM recette r
+WHERE r.id_recette NOT IN (
+    SELECT r.id_recette
+    FROM ingredientparrecette ir 
+    INNER JOIN ingredient i
+	ON i.id_ingredient = ir.id_ingredient
+    WHERE i.nomIngredient IS NULL
+);
+
+cette requette me renvoi toutes les recettes sans exception
+OU 
+
+SELECT r.nomRecette
+FROM recette r
+LEFT JOIN ingredientparrecette ir 
+ON r.id_recette = ir.id_recette
+LEFT JOIN ingredient i 
+ON ir.id_ingredient = i.id_ingredient
+WHERE ir.id_ingredient IS NULL;
+
+cette requette me renvoi uniquement la recette eau chaude
+
+
+-- 18 Trouver les ingrédients qui sont utilisés dans au moins 3 recettes
+SELECT ir.id_ingredient, nomIngredient
+FROM ingredientparrecette ir
+INNER JOIN ingredient i
+ON i.id_ingredient = ir.id_ingredient
+GROUP BY i.nomIngredient
+HAVING COUNT(DISTINCT ir.id_recette) >= 3;
+
+renvoi id 21 nom oeufs
+
+-- 19 Ajouter un nouvel ingrédient à une recette spécifique
+UPDATE ingredient i
+SET i.nomIngredient = 'boeuf'
+INNER JOIN recette r
+ON r.id_ingredient = i.id_ingredient
+WHERE id_recette = 10;
+
+pb...a corriger
+
+-- 20 Bonus : Trouver la recette la plus coûteuse de la base de données (il peut y avoir des ex aequo, il est 
+-- donc exclu d’utiliser la clause LIMIT)
