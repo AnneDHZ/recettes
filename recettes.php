5486ee275f72aaaff5f5a49367fd9d78127f5f6a
AnneDHZ <anne.dehaeze@yahoo.com>
2024-12-12 08:56:45 +0100

    tableau html pour recettes.php

--- recettes.php
+++ recettes.php
@@ -29,17 +29,25 @@ $declarationRecette->execute();
 $recettes = $declarationRecette->fetchAll();
 ?>
 
-
+<table border=1>
+	<thead>
+		<tr>
+			<th>Recette</th>
+			<th>Catégorie</th>
+			<th>Temps de préparation</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
 <?php
 // On affiche chaque recette une à une avec les details:nom, catégorie et temps de préparation
 foreach ($recettes as $recette) {
 ?>
-
-    <p>
-		<a href="detailsRecette.php?id=<?= $recette['id_recette'] ?>" class="lien"><?= $recette['nomRecette']; ?></a>, 
-		<?= $recette['nomCategorie']; ?>, 
-		<?= $recette['tempsPrepa']; ?> minutes
-	</p>
+			<td><a href="detailsRecette.php?id=<?= $recette['id_recette'] ?>" class="lien"><?= $recette['nomRecette']; ?></a></td>
+			<td><?= $recette['nomCategorie']; ?></td>
+			<td><?= $recette['tempsPrepa']; ?> minutes</td>
+		</tr>
+	</tbody>
 	
 <?php
 }
